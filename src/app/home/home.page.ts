ca9f3c8978605b847fe35856631ff50d5236ee41
Mohamed-Talaat <eng.muhamedtalaat@gmail.com>
2023-06-08 21:25:32 +0300

    tested on android

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BleClient } from '@capacitor-community/bluetooth-le';
 import { filter } from 'rxjs';
 import { BLE } from '@ionic-native/ble/ngx';
+import { LoadingController } from '@ionic/angular';
   
 @Component({
   selector: 'app-home',
@@ -9,30 +10,88 @@ import { BLE } from '@ionic-native/ble/ngx';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  deviceName:any
-  deviceID:any
-  deviceUUID:any
-  constructor(private ble: BLE) { }
+  deviceName:any;
+  deviceID:any;
+  deviceUUID:any;
+  errorMessage :any= null;
+  constructor(private ble: BLE,private loadingController: LoadingController) { }
   
-  // async scanDevices(){
-  //   await BleClient.initialize({ androidNeverForLocation: true });
-  //   const device = await BleClient.requestDevice()
-  //   console.log(device)
-  //   this.deviceID = device.deviceId
-  //   this.deviceName = device.name
-  //   this.deviceUUID = device.uuids
-  // }
+  // async scanDevices() {
+  //   await BleClient.initialize();
+  
+  //   // Create and present the loading spinner
+  //   const loading = await this.loadingController.create({
+  //     message: 'Scanning for devices...'
+  //   });
+  //   await loading.present();
+  
+  //   // Start the BLE scan
+  //   const scanSubscription = await BleClient.requestLEScan({ namePrefix: "iTAG" }, (result) => {
+  //     console.log(result.localName)
+  //     console.log(result.uuids)
+  //     if (result.localName === "iTAG") {
+  //       this.deviceID = result.manufacturerData
+  //       this.deviceName = result.localName
+  //       this.deviceUUID = result.uuids
+  
+  //       // Stop scanning
+  //     }
+  //   })
+  //   // await BleClient.stopLEScan();
 
-  scanDevices() {
+  //   // // Dismiss the loading spinner
+  //   // await loading.dismiss();
+  
+  //   // If scan doesn't find device after a set amount of time, stop scanning and dismiss loader
+  //   setTimeout(async () => {
+  //     if (this.deviceID === null) {
+  //       await BleClient.stopLEScan();
+  //       await loading.dismiss();
+  //     }
+  //   }, 10000);  // Adjust the timeout as needed
+  // }
+  
+
+  async scanDevices() {
+    const loading = await this.loadingController.create({
+      // message: 'Scanning for devices...'
+    });
+    await loading.present();
+  
     this.ble.startScan([]).subscribe(
       device => {
-        if (device.id === "FF:FF:90:06:86:0D") {
-          console.log(JSON.stringify(device));
-          this.deviceID = device.id;
-          this.deviceName = device.name;
-        }
+        // this.deviceID = null
+        // this.deviceName = null
+        // this.deviceUUID = null
+        console.log('--------- device id --------')
+        console.log(device.id);
+        console.log('--------- device name --------')
+        console.log(device.name);
+        // if (device.id === "FF:FF:AA:A3:E8:C2") {
+        // if (device.id === "FF:FF:AA:A3:E8:C2") {
+        //   console.log(device.id);
+        //   this.deviceID = device.id;
+        //   this.deviceName = device.name;
+        //   this.deviceUUID = device.uuids
+        //   this.ble.stopScan();
+        //   loading.dismiss();  // Dismiss the loading spinner when the device is found
+        // } else if(device.id === "FF:FF:90:06:86:0D")
+        // {
+        //   console.log(device.id);
+        //   this.deviceID = device.id;
+        //   this.deviceName = device.name;
+        //   this.deviceUUID = device.uuids
+        //   this.ble.stopScan();
+        //   loading.dismiss();
+        // }
+      },
+      error => {
+        console.error(JSON.stringify(error));
+        this.errorMessage = JSON.stringify(error.Msg)
+        loading.dismiss();  // Dismiss the loading spinner when an error occurs
       }
-    );    
+    );
   }
+  
 
 }
